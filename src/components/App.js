f31394edd12d0fb8cce0faa0e0b0a419c85cdaf0
Hamdiye Tunc <whamdiyetunc@outlook.com>
2023-09-14 14:40:44 +0300

    file added

--- src/components/App.js
+++ src/components/App.js
@@ -5,40 +5,30 @@ import Users from './Users';
 import Search from './Search';
 import Alert from './Alert';
 import About from './About';
-import axios from 'axios';
 import UserDetails from './UserDetails'
 import GithubState from '../context/github/githubState'
+import AlertState from '../context/alert/alertState';
 
 const App = () => {
-  const [alert, setAlert] = useState (null)
-
-  const showAlert = (msg, type) => {
-    setAlert({msg,type})
-
-    setTimeout(() => {
-      setAlert({alert: null});
-    }, 3000);
-  }
-
   return (
     <GithubState>
-      <BrowserRouter>
-        <Navbar />
-        <Alert alert={alert}/>
-        <Switch>
-              <Route exact path="/" render={ props => (
-                  <>
-                    <Search 
-                      setAlert={showAlert}
-                      />
-                    <Users />
-                  </>
-                )
-              }/>
-              <Route path="/about" component={About} />
-              <Route path="/user/:login" component={UserDetails}/>
-        </Switch>
-      </BrowserRouter>
+        <AlertState>
+            <BrowserRouter>
+                <Navbar />
+                <Alert/>
+                <Switch>
+                      <Route exact path="/" render={ props => (
+                          <>
+                            <Search />
+                            <Users />
+                          </>
+                        )
+                      }/>
+                      <Route path="/about" component={About} />
+                      <Route path="/user/:login" component={UserDetails}/>
+                </Switch>
+            </BrowserRouter>
+        </AlertState>
     </GithubState>
   )
 }
